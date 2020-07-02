a6a6c756733c668123fcbcde22cb5526e977843e
abhirama24 <51402435+abhirama24@users.noreply.github.com>
2020-07-02 15:43:06 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+  <?php include('templates/h.php'); ?>
+  <div class="aa ">
+  	<h4 class="center red-text"> Welcome to Our School</h4>
+  	
+  	<h5 class="center red-text">Login as</h5>
+  	<a href="students.php" class="btn brand-text"> Student</a>
+
+  	<br><br><br>
+
+     <a href="staff.php" class="btn brand-text"> Staff</a>
+  </div>
+  <?php include('templates/f.php'); ?>
+
+ 
+ 
+ </html>
