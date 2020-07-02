7fc26e3c70cea46d0f031fb7d033c8a6524175af
abhirama24 <51402435+abhirama24@users.noreply.github.com>
2020-07-02 15:43:58 +0530

    Create staff.php

---
+++ staff.php
@@ -0,0 +1,30 @@
+<?php 	
+
+
+	
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ 
+ <?php 	include('templates/h.php'); ?>
+ <h3 class="center green-text">	Staff Portal</h3>
+
+ 	<div class="as">	
+
+	<a href="staff_new_user.php" class="btn brand-text"> New User</a>
+	<br><br>
+ 	<a href="staff_existing.php" class="btn brand-text"> Existing User</a>
+
+ 	<br><br><br><br>
+ 	<hr>
+
+ 	
+
+
+ 	</div>
+ 	
+
+
+ </html>
