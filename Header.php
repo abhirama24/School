1ea3433f78cf73ae02fe4c55b37c7d37be5fd71e
abhirama24 <51402435+abhirama24@users.noreply.github.com>
2020-07-07 23:43:31 +0530

    Create Header.php

---
+++ Header.php
@@ -0,0 +1,72 @@
+<head>
+ 	<title>xyz school Online protal</title>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<style type="text/css">
+	.aa
+	{
+		margin: 20px auto;
+		max-width: 600px;
+		padding: 50px;
+		background-color: white;
+		text-align: center;
+	}
+	.brand{
+		color: red;
+	}
+	body{
+		background-color: lightblue;
+	}
+	.as{
+		margin: 20px auto;
+		max-width: 500px;
+		padding: 50px;
+		background-color: white;
+		text-align: center;
+		size: 45px;
+
+	}
+	.ab{
+		font-size: 25px;
+		text-indent: left;
+		
+	}
+	.abhi{
+		background-color: red;
+		height: 40px;
+		text-align: center;
+	}
+	.ac{
+		margin: 20px auto;
+		max-width: 600px;
+		padding: 50px;
+		background-color: white;
+		text-align: center;
+	}
+	.ad{
+		text-align: center;
+		text-decoration-style: dashed;
+		font-size: 25px;
+	}
+	.ae
+	{
+		font-size: 40px;
+		text-decoration: underline;
+        text-decoration-style:dashed;
+        color: gold;
+	}
+	table,th,td
+	{
+		 border: 1px solid black;
+
+	}
+
+</style>
+ </head>
+ <body>
+
+ 	<nav class=" center">
+ 		<div class="container">
+ 			XYZ school Online Portal
+ 		</div>
+ 	</nav>
